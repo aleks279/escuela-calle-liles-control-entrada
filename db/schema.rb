f0568438282c38f003af773208f1d50951f67009
Saul Zamora <szamora@pernix-solutions.com>
2019-08-30 10:59:02 -0600

    Add user name/id number to registries to avoid problems after deleting a user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_180642) do
+ActiveRecord::Schema.define(version: 2019_08_30_162450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_01_21_180642) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.string "user_id_number"
     t.index ["user_id"], name: "index_registries_on_user_id"
   end
 
